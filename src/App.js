7bffadaedb8d947315ad1e7a3521bdc2eda23902
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-23 22:34:38 +0530

    Routing part 1 with children routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter as Router, RouterProvider } from "react-router";
+import { createBrowserRouter as Router, RouterProvider, Outlet } from "react-router";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
@@ -11,7 +11,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
-            <Body/>
+            <Outlet/>
         </div>
     )
 }
@@ -20,15 +20,21 @@ const appRouter = Router([
   {
     path:"/",
     element:<AppLayout/>,
+    children: [
+      {
+        path:"/",
+        element:<Body/>
+      },
+      {
+        path:"/about",
+        element:<About/>
+      },
+      {
+        path:"/contact",
+        element:<Contact/>
+      }
+    ],
     errorElement:<Error/>
-  },
-  {
-    path:"/about",
-    element:<About/>
-  },
-  {
-    path:"/contact",
-    element:<Contact/>
   }
 ]);
 
