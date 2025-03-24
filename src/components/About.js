9dda78469ef4683dbdd63e52a2c28595090cefbe
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-24 21:30:23 +0530

    Class Based Component episode 8

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return(
         <div>
             <h1>About</h1>
             <h2>This is the About Page</h2>
+            <UserClass name={"Akriti (class)"}/>
         </div>
     )
 }
---
+++ src/components/UserClass.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+class UserClass extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count : 0
+        }
+    }
+
+    render(){
+        return (
+            <div className="user-card">
+            <h1>Count: {this.state.count}</h1>
+            <button onClick ={() =>{
+                this.setState({
+                    count:this.state.count + 1
+                })
+            }}>Count Increase</button>
+            <h2>Name: {this.props.name}</h2>
+            <h3>Location: BLR</h3>
+            <h4>Contact: @akriti</h4>
+            </div>
+        )
+    }
+}
+
+export default UserClass;
