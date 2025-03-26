9047abc5483db2acb151b71271d96a66e7ad2d49
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-26 22:09:10 +0530

    Redux and creating cart component episode 12

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,15 @@ import { useState , useEffect, useContext} from "react";
 import { Link } from "react-router";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
+
 const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState("Login");
     const onlineStatus = useOnlineStatus();
 
     const data = useContext(UserContext);
+
+    const cartItems = useSelector((store) => store.cart.items);
 
     return (
         <div className="flex justify-between bg-pink-100 shadow-lg">
@@ -21,7 +25,7 @@ const Header = () => {
                     <li className="px-4"><Link to ="/about">About Us</Link></li>
                     <li className="px-4"><Link to ="/contact">Contact Us</Link></li>
                     <li className="px-4"><Link to ="/grocery">Grocery</Link></li>
-                    <li className="px-4"><Link to ="/cart">Cart</Link></li>
+                    <li className="px-4 font-bold text-xl"><Link to ="/cart">Cart ({cartItems.length} items)</Link></li>
                     <button className="Login w-20 px-4 bg-pink-300 border-black rounded-lg" onClick={() => btnNameReact === "Login" ? setBtnNameReact("Logout") : setBtnNameReact("Login")}>{btnNameReact}</button>
                     <li className="px-4 font-bold">{data.loggedInUser}</li>
                 </ul>
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,6 +1,16 @@
 import { CDN_URL } from "../utils/constants";
+import { useDispatch} from "react-redux";
+import { addItem } from "../utils/cartSlice";
 
 const ItemList = ({items}) => {
+
+    const dispatch = useDispatch();
+
+    const handleAddItem = (item) => {
+        //Dispatch an action
+        dispatch(addItem(item))
+    }
+
     return (
         <div>
             {items.map((item) => (
@@ -8,13 +18,16 @@ const ItemList = ({items}) => {
                     <div className="w-9/12">
                         <div className="py-2">
                             <span>{item.card.info.name}</span>
-                            <span> - ₹{item.card.info.defaultPrice / 100}</span>
+                            <span> - ₹{item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice / 100}</span>
                         </div>
                         <p className="text-xs">{item.card.info.description}</p>
                     </div>
                     <div className="w-3/12 p-4">
                         <div className="absolute">
-                            <button className="p-2 rounded-lg bg-black text-white shadow-lg">Add +</button>
+                            <button className="p-2 rounded-lg bg-black text-white shadow-lg cursor-pointer"
+                            onClick = {() => handleAddItem(item)}>
+                                Add +
+                            </button>
                         </div>    
                         <img src={CDN_URL + item.card.info.imageId}/>        
                     </div>
