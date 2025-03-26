e8097a57aa8b7f364b1429785e11f36c52d95eb1
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-26 17:04:57 +0530

    Lifting state up episode 11

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import ItemList from "./ItemList";
 
-const RestaurantCategory = ({data}) => {
-
-    const [showItems, setShowItems] = useState(false);
+const RestaurantCategory = ({data, showItems, setShowIndex}) => {
 
     const handleClick = () => {
-        setShowItems(!showItems);
-    }
-
+        setShowIndex();
+    }; 
+     
     console.log(data)
     return (
         <div>
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,6 +2,7 @@ import Shimmer from "./Shimmer";
 import { useParams } from "react-router";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
 import RestaurantCategory from "./RestaurantCategory";
+import { useState } from "react";
 
 const RestaurantMenu = () =>{
 
@@ -9,25 +10,29 @@ const RestaurantMenu = () =>{
     console.log(resId);
 
     const resInfo = useRestaurantMenu(resId);
+    const [showIndex, setShowIndex] = useState(0);
 
-if (resInfo === null){
-     return <Shimmer/>;
-}
+    if (resInfo === null){
+        return <Shimmer/>;
+    }
 
-const {name, cuisines, costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
+    const {name, cuisines, costForTwoMessage} = resInfo?.cards[2]?.card?.card?.info;
 
-const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-console.log(itemCards);
-console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
-const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=> c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
-console.log(categories);
+    const {itemCards} = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+    console.log(itemCards);
+    console.log(resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards);
+    const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c=> c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
+    console.log(categories);
 
     return (
         <div className="text-center">
             <h1 className="font-bold my-6 text-2xl">{name}</h1>
             <h2 className="font-bold text-lg">{cuisines.join(', ')}</h2>
             <h2 className="font-bold text-lg">{costForTwoMessage}</h2>
-            {categories.map((category) => (<RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}/>))}
+            {categories.map((category, index) => (<RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}
+            showItems={index === showIndex && true}
+            setShowIndex={() => setShowIndex(index)}
+            />))}
         </div>
     )
 }
