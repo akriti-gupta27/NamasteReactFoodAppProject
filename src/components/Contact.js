ec87e5bc0979e12649aa78a12b711ce178cdcbb5
akriti-gupta27 <akritigupta8596@gmail.com>
2025-03-27 14:31:12 +0530

    First Test case for contact us page episode 13

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,13 @@
 const Contact = () => {
     return(
-        <div>Contact US</div>
+        <div> 
+            <h1 className="font-bold text-3xl p-4 m-4">Contact US</h1>
+            <form>
+                <input type="text" className="border border-black p-2 m-2" placeholder="name"></input>
+                <input type="text" className="border border-black p-2 m-2" placeholder="message"></input>
+                <button className="border border-black p-2 m-2 bg-gray-200 rounded-xl">SUBMIT</button>
+            </form>        
+        </div>       
     )
 }
 
